aff32d0d40f5365e3a0f65aeba18692ae15fefa0
g1atex <cihatduger@gmail.com>
2023-01-26 19:38:25 +0300

    task 4

--- src/day11/Producer.java
+++ src/day11/Producer.java
@@ -3,7 +3,7 @@ package day11;
 public class Producer {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello world");
+        System.out.println("helloworld");
         System.out.println("nevak gönülllllll");
     }
 }
